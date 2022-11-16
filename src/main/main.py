e2e0aeed0cf7931361a3c00464141da846dcee34
Apurva Waghmode <apurva.waghmode@clairvoyantsoft.com>
2022-11-16 17:15:07 +0530

    Code reformat

--- src/main/main.py
+++ src/main/main.py
@@ -1,11 +1,10 @@
 from pyspark.sql import SparkSession
 import configparser
-from ingestion import upload_data, create_s3_bucket
+from ingestion import upload_raw_data, create_s3_bucket
 
 # Reading configs
 config = configparser.ConfigParser()
-config.read("../pythonProject/config.ini")
-
+config.read(r"../PycharmProjects/upskill_project/config_file/config.ini")
 
 def create_dataframe(spark):
     df = spark.read. \
@@ -21,7 +20,7 @@ def create_dataframe(spark):
 if __name__ == '__main__':
     spark = SparkSession \
         .builder.config("spark.jars",
-                        'C:/Users/Apurva Waghmode/PycharmProjects/pythonProject/Jar_files/aws-java-sdk-bundle-1.11.563.jar,C:/Users/Apurva Waghmode/PycharmProjects/pythonProject/Jar_files/hadoop-aws-3.2.2.jar,C:/Users/Apurva Waghmode/PycharmProjects/pythonProject/Jar_files/mysql-connector-java-8.0.22.jar') \
+                        '../PycharmProjects/pythonProject/Jar_files/aws-java-sdk-bundle-1.11.563.jar,../PycharmProjects/pythonProject/Jar_files/hadoop-aws-3.2.2.jar,../PycharmProjects/pythonProject/Jar_files/mysql-connector-java-8.0.22.jar') \
         .config('spark.hadoop.fs.s3a.aws.credentials.provider', 'org.apache.hadoop.fs.s3a.SimpleAWSCredentialsProvider') \
         .config('spark.hadoop.fs.s3a.access.key', config.get('aws', 'AWS_ACCESSKEY')) \
         .config('spark.hadoop.fs.s3a.secret.key', config.get('aws', 'AWS_SECRETKEY')) \
@@ -29,6 +28,5 @@ if __name__ == '__main__':
         .getOrCreate()
 
     a = create_dataframe(spark)
-    path = "s3a://aws-s3-bucket-123/"
-    create_s3_bucket(config.get('aws', 'AWS_BUCKETNAME'), config.get('aws', 'AWS_ACCESSKEY'),config.get('aws', 'AWS_SECRETKEY'))
-    upload_data(a, path)
+    create_s3_bucket(config.get('aws','bucket_name'), config.get('aws', 'AWS_ACCESSKEY'), config.get('aws', 'AWS_SECRETKEY'))
+    upload_raw_data(a, config.get('aws', 'path'))
